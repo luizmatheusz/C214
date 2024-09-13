8dc4c50d8197b02bfefc6163646ba054fe956589
Luiz <lmatheus.rod@gmail.com>
2024-09-13 20:17:20 -0300

    Correção no atendimento.py

--- atendimento.py
+++ atendimento.py
@@ -1,11 +1,10 @@
 import requests
 
-def calc_predio(response):
-    nPredio = (int(response['sala'])-1)/5
-    return nPredio
+def calc_predio(nPredio):
+    return (nPredio-1)/5
 
 def get_atendimento():
     response = requests.get(f"http://api.inatel.com/atendimento").json() # URL hipotética para obtenção das informações
-    nPredio = calc_predio(response) # Cálculo do número do prédio com base no número da sala
+    nPredio = calc_predio(int(response['sala'])) # Cálculo do número do prédio com base no número da sala
     response['predio'] = [nPredio]
     return response
